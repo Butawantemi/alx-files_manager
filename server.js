70a94dfc67b805cdfc176f7f5747c0877de032f0
Butawantemi <japhetbuta@gmail.com>
2024-06-18 19:13:10 +0300

    add files controller

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
+import router from './routes/index';
+
 const express = require('express');
 
 const app = express();
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 5000;
+
+router(app);
 
 app.listen(port, () => {
   console.log(`server is running on port ${port}`);
